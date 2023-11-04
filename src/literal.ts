2314839aa4e4005c0a93ab2a0b1a3e86383634eb
kilig <kilig0909@gmail.com>
2023-11-04 14:44:36 +0900

    literal つづき

--- src/literal.ts
+++ src/literal.ts
@@ -2,4 +2,10 @@ export {};
 
 let dayOfTheWeek: '日' | '月' | '火' | '水' | '木' | '金' | '土' = '日';
 dayOfTheWeek = '月';
-dayOfTheWeek = '31'; // コンパイルエラー
+// dayOfTheWeek = '31'; // コンパイルエラー
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 = 12;
+// month = 13; // コンパイルエラー
+
+let TRUE: true = true;
+// TRUE = false; // コンパイルエラー
